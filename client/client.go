8d4f83e2b6d9ce97f2d48656aadb6b7f467df203
Kris-4321 <kristianpanev@gmail.com>
2024-02-22 09:41:35 +0200

    update 22.02

--- client/client.go
+++ client/client.go
@@ -4,77 +4,176 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
+
+	pb "mongodbtest/proto"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-
-	pb "mongodbtest/proto"
-)
-
-const (
-	address = "localhost:50051"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func main() {
-	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("didnt connect %v", err)
+		log.Fatalf("couldnt connect %v", err)
 	}
 	if err == nil {
-		fmt.Println("connection successful")
+		fmt.Println("connected successfully")
 	}
-	defer conn.Close()
 
+	defer conn.Close()
 	client := pb.NewUserManagerClient(conn)
 
-	fmt.Print(`Choose an option:
-1 create user
-2 read user
-3 update user
-4 delete user`)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 
+	fmt.Print(`
+	select an option:
+	1 create new user
+	2 Search for user
+	3 Update user
+	4 delete user
+	5 list all users
+	choice: `)
 	var choice int
-	_, err1 := fmt.Scan(&choice)
-	if err1 != nil {
-		log.Fatal(err)
+	_, err3 := fmt.Scan(&choice)
+	if err3 != nil {
+		log.Fatalf("error : %v", err3)
 	}
 
 	switch choice {
 	case 1:
-		var id int32
-		_, err2 := fmt.Scan(&id)
-		if err2 != nil {
-			fmt.Print(err2)
-		}
-
-		var name string
-		_, err3 := fmt.Scan(&name)
-		if err3 != nil {
-			fmt.Print(err3)
-		}
-
-		var email string
-		_, err4 := fmt.Scan(&email)
-		if err4 != nil {
-			fmt.Print(err4)
-		}
-
-		var password string
-		_, err5 := fmt.Scan(&password)
-		if err5 != nil {
-			fmt.Print(err5)
-		}
-
-		newuser := pb.User{Id: id, Name: name, Email: email, Password: password}
-
-		createusr, err := client.Create(context.Background(), &pb.CreateRequest{User: &newuser})
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(createusr.CreateResponse)
+		createUser(client, ctx)
 
 	case 2:
+		readUser(client, ctx)
 
+	case 3:
+		updateUser(client, ctx)
+
+	case 4:
+		deleteUser(client, ctx)
+
+	case 5:
+		readallUsers(client, ctx)
+	}
+}
+
+func createUser(client pb.UserManagerClient, ctx context.Context) {
+	var name, email, password string
+
+	fmt.Print("enter name:")
+	_, err := fmt.Scanln(&name)
+	if err != nil {
+		log.Fatalf("failed reading name %v", err)
 	}
 
+	fmt.Print("enter email:")
+	_, err2 := fmt.Scanln(&email)
+	if err2 != nil {
+		log.Fatalf("failed reading email %v", err2)
+	}
+
+	fmt.Print("enter password:")
+	_, err3 := fmt.Scanln(&password)
+	if err3 != nil {
+		log.Fatalf("failed reading password %v", err3)
+	}
+
+	timeout := 30 * time.Second
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	user, err4 := client.Create(ctx, &pb.CreateRequest{
+		Name:     name,
+		Email:    email,
+		Password: password,
+	})
+
+	if err4 != nil {
+		log.Printf("failed adding user %v", err4)
+	}
+	log.Printf("added user %v", user)
+}
+
+func readUser(client pb.UserManagerClient, ctx context.Context) {
+	var id string
+	fmt.Print("enter id:")
+	_, err := fmt.Scanln(&id)
+	if err != nil {
+		log.Fatalf("failed reading id %v", err)
+	}
+
+	user, err := client.Read(ctx, &pb.ReadRequest{
+		Id: id,
+	})
+	if err != nil {
+		log.Fatal("couldnt find user", err)
+	}
+	fmt.Printf("Read user: %v", user)
+}
+
+func updateUser(client pb.UserManagerClient, ctx context.Context) {
+	var id, name, email, password string
+
+	fmt.Print("enter id:")
+	_, err0 := fmt.Scanln(&id)
+	if err0 != nil {
+		log.Fatalf("failed reading id %v", err0)
+	}
+
+	fmt.Print("enter name:")
+	_, err := fmt.Scanln(&name)
+	if err != nil {
+		log.Fatalf("failed reading name %v", err)
+	}
+
+	fmt.Print("enter email:")
+	_, err2 := fmt.Scanln(&email)
+	if err2 != nil {
+		log.Fatalf("failed reading email %v", err2)
+	}
+
+	fmt.Print("enter password:")
+	_, err3 := fmt.Scanln(&password)
+	if err3 != nil {
+		log.Fatalf("failed reading password %v", err3)
+	}
+
+	user, err4 := client.Update(ctx, &pb.UpdateRequest{
+		Id:       id,
+		Name:     name,
+		Email:    email,
+		Password: password,
+	})
+	if err4 != nil {
+		log.Fatalf("couldnt update user %v", err4)
+	}
+	log.Printf("user updated %v", user)
+}
+
+func deleteUser(client pb.UserManagerClient, ctx context.Context) {
+	var id string
+	fmt.Print("enter id:")
+	_, err0 := fmt.Scanln(&id)
+	if err0 != nil {
+		log.Fatalf("failed reading id %v", err0)
+	}
+
+	_, err := client.Delete(ctx, &pb.ReadRequest{
+		Id: id,
+	})
+	if err != nil {
+		log.Fatalf("couldnt delete user with id %v", err)
+	}
+	log.Println("deleted user:")
+}
+
+func readallUsers(client pb.UserManagerClient, ctx context.Context) {
+	users, err := client.ReadAll(ctx, &emptypb.Empty{})
+	if err != nil {
+		log.Fatalf("couldnt list users %v", err)
+	}
+	log.Printf("Users: %v", users)
 }
--- server/server.go
+++ server/server.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	pb "mongodbtest/proto"
+	"net"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+const (
+	port           = ":50051"
+	dbname         = "mongotestdb"
+	collectionname = "users"
 )
 
 type UserManagmentServer struct {
@@ -11,7 +26,70 @@ type UserManagmentServer struct {
 	mongoClient *mongo.Client
 }
 
-const (
-	dbname = ":"
-	port   = ":50051"
-)
+func (s *UserManagmentServer) Create(ctx context.Context, in *pb.CreateRequest) (*pb.UserResponse, error) {
+	collection := s.mongoClient.Database(dbname).Collection(collectionname)
+	result, err := collection.InsertOne(ctx, bson.M{"name": in.GetName(), "email": in.GetEmail(), "password": in.GetPassword()})
+	if err != nil {
+		log.Printf("failed to add user into mongo %v", err)
+		return nil, err
+	}
+
+	oid, err2 := result.InsertedID.(primitive.ObjectID)
+	if !err2 {
+		log.Printf("failed to add id %v", err2)
+		return nil, fmt.Errorf("failed to convert id to ojectid %v", err2)
+	}
+
+	return &pb.UserResponse{Id: oid.Hex(), Name: in.GetName(), Email: in.GetEmail(), Password: in.GetPassword()}, nil
+}
+
+func (s *UserManagmentServer) Read(ctx context.Context, in *pb.ReadRequest) (*pb.UserResponse, error) {
+	collection := s.mongoClient.Database(dbname).Collection(collectionname)
+	ID, err := primitive.ObjectIDFromHex(in.GetId())
+	if err != nil {
+		return nil, err
+	}
+	var user bson.M
+	if err := collection.FindOne(ctx, bson.M{"_id": ID}).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &pb.UserResponse{Id: in.GetId(), Name: user["name"].(string), Email: user["email"].(string), Password: user["password"].(string)}, nil
+}
+
+func (s *UserManagmentServer) Readall(ctx context.Context, in *emptypb.Empty) (*pb.ReadAllResponse, error) {
+	collection := s.mongoClient.Database(dbname).Collection(collectionname)
+	cursor, err := collection.Find(ctx, bson.D{{}})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+	var users []*pb.UserResponse
+	for cursor.Next(ctx) {
+		var user bson.M
+		if err = cursor.Decode(&user); err != nil {
+			return nil, err
+		}
+		id := user["_id"].(primitive.ObjectID).Hex()
+		users = append(users, &pb.UserResponse{Id: id, Name: user["name"].(string), Email: user["email"].(string), Password: user["password"].(string)})
+
+	}
+	return &pb.ReadAllResponse{Users: users}, nil
+}
+
+func main() {
+	mongoClient, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatalf("couldnt connect to mongo %v", err)
+	}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("couldnt listen %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterUserManagerServer(s, &UserManagmentServer{mongoClient: mongoClient})
+	log.Printf("server listening on %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server %v", err)
+	}
+}
